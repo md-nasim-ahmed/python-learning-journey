d9459ffaf4426e79d0812a695277a44a9ae03d24
Nasim <mdnasimahmed341@gmail.com>
2026-08-29 18:27:12 +0600

    feat: declare multiple variables

--- variables.py
+++ variables.py
@@ -5,3 +5,23 @@ height =5.8
 print (user_full_name)
 print (userAge)
 print (height)
+
+
+# Many Values to Multiple Variables
+a,b,c = "Bangladesh", "India", "China"
+print (a)
+print (b)
+print (c)
+
+# One Value to Multiple Variables
+x = y = z = "Bangladesh"
+print (x)
+print (y)
+print (z)
+
+# Unpack Collection
+countries = ["Bangladesh", "India", "China"]
+e,f,g = countries
+print (e)
+print (f)
+print (g)
