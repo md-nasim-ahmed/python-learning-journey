72d8c5568c39fd6807fe4c4691ff423438f5f24c
Nasim <mdnasimahmed341@gmail.com>
2026-08-31 15:43:10 +0600

    feat: declare numeric data types

--- data_types.py
+++ data_types.py
@@ -71,3 +71,15 @@ print(type(z))
 #memoryview
 x = memoryview(bytes(1))
 print(type(x))
+
+
+
+"""Numeric Data Types"""
+age = 27 #int data types
+print (age)
+
+marks = 80.5 #float data types
+print (marks)
+
+complex_number = 10 + 20j #complex data types
+print (complex_number)
