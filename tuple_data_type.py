72f02c4ab41dae66c5d6fbd553850ac5c5f948b0
Nasim <mdnasimahmed341@gmail.com>
2026-09-02 17:03:47 +0600

    feat: Sequence Data Types - tuple data type practice

---
+++ tuple_data_type.py
@@ -0,0 +1,22 @@
+""" Sequence Data Types """
+
+# tuple data type - immutable
+coordinates = (23.8103, 90.4125)
+user_info = ("Nasim", 27, "Web Developer", True)
+print(coordinates)
+
+latter = ("A", "B", "C", "D")
+mixed_value = ("A","B",2,2.2)
+print(mixed_value)
+
+
+# common tuple operations
+colors = ("red", "green", "blue", "yellow")
+print(colors[0])
+print(colors[-1])
+print (colors[1:3])
+
+
+# checking length
+colors = ("red", "green", "blue", "yellow")
+print(len(colors))
