93fe3bf6ab45d504f29692b1a84b694191813c85
Nasim <mdnasimahmed341@gmail.com>
2026-09-10 16:39:54 +0600

    feat: string indexing practice

---
+++ string_indexing.py
@@ -0,0 +1,13 @@
+""" String Indexing - Immutable """
+
+# Positive Indexing (Start = 0,1,2,3,5....)
+positive_ind = "Python"
+print(positive_ind[0])
+print(positive_ind[3])
+print(positive_ind[5])
+
+# Negative Indexing (Last = -1,-2,-3,-4,-5... )
+negative_ind = "Python"
+print(negative_ind[-2])
+print(negative_ind[-4])
+print(negative_ind[-6])
