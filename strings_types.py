750575181b932cc0790f5a773bfbbc19a77722e7
Nasim <mdnasimahmed341@gmail.com>
2026-09-10 16:27:37 +0600

    feat: string types practice

---
+++ strings_types.py
@@ -0,0 +1,24 @@
+"""single, double, and multiple Python Strings"""
+
+# Single Quote Strings('...')
+single_str = 'Hello, Python!'
+print(single_str)
+
+quote = 'He said, "Python is awesome"'
+print(quote)
+
+
+# Double Quote Strings ("...")
+double_str = "Hello, Python!"
+print(double_str)
+
+message = "It's a beautiful day for coding!"
+print(message)
+
+
+# Multiple Line Strings ('''...''' / """...""")
+multiline_str = """ This is a multi-line string in Python.
+You can write text across multiple lines
+without using any newline(\\n) characters"""
+print(multiline_str)
+
