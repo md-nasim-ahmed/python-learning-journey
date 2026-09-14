befdfeb5b257e0d5b53b533ee017a8bce75e29c4
Nasim <mdnasimahmed341@gmail.com>
2026-09-14 17:55:17 +0600

    feat: string repetition practice

---
+++ string_repetition.py
@@ -0,0 +1,15 @@
+""" Repetition """
+
+word = "Python "
+result = word * 3
+print(result)
+
+# Console UI Formatting
+print("=" * 40)
+print ("    Welcome to My Python Program    ")
+print("=" * 40)
+
+print("*" * 1)
+print("*" * 2)
+print("*" * 3)
+print("*" * 4)
