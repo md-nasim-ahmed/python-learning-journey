2475be826696ebb6f12d3018a3abd03256e4cc5a
Nasim <mdnasimahmed341@gmail.com>
2026-09-15 17:55:58 +0600

    feat: string split and case conversion practice

---
+++ string_split_case.py
@@ -0,0 +1,22 @@
+""" String Split Case """
+
+# split()
+text = "Is It Case Conversion?"
+
+words_list = text.split(" ")
+print(words_list)
+
+
+# lower().split()
+text = "Is It Case Conversion?"
+
+lower_list = text.lower().split()
+print(lower_list)
+
+
+# upper().split()
+text = "Is It Case Conversion?"
+
+upper_list = text.upper().split()
+print(upper_list)
+
