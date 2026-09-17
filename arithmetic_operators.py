1fe6785aac6118eb46a70a75068b46f857d56f1b
Nasim <mdnasimahmed341@gmail.com>
2026-09-17 18:44:52 +0600

    feat: basic arithmetic operators practice

---
+++ arithmetic_operators.py
@@ -0,0 +1,25 @@
+""" Basic Arithmetic Operators """
+
+a = 15
+b = 4
+
+print ("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:",a*b)
+print("Division:", a/b)
+print("Floor Division:", a//b)
+print("Modulus:", a%b)
+print("Exponentiation:", a**b)
+
+
+"""
+Operator Precedence
+
+1) () — Parentheses
+2) ** — Exponentiation
+3) *, /, //, % — 
+4) +, - — 
+
+"""
+result = 10 + 2 * 3 ** 2
+print(result)
