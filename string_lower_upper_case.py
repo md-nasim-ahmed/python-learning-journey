dbecc47e25853f41e42826f24313a6a0257d1767
Nasim <mdnasimahmed341@gmail.com>
2026-09-17 18:07:17 +0600

    feat: string lowercase and uppercase practice

---
+++ string_lower_upper_case.py
@@ -0,0 +1,14 @@
+"""String Lowercase and Uppercase"""
+
+sentence = "this is a sample sentence"
+print(sentence.title())
+
+
+# Remove space and make it uppercase
+text = " hello world "
+print(text.strip().upper())
+
+
+# Make lower case
+text = "Python Developer"
+print(text.lower())
